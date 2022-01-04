9ad861de0a8000253278c374f8e49bc58c88a3d4
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-04 18:20:42 -0300

    Criacao aula03.cpp

---
+++ aulas - parte 1/aula 03 - Estrutura basica/aula03.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "Ola mundo!" << endl;// cout - imprimi na tela , endl pula linha ou \n dentro da string
+
+    system("pause");
+    return 0;
+}
