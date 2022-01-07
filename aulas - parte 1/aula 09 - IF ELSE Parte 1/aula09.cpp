aeee236e572fa1cec3f566d23948cb26d3e16145
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-07 17:33:20 -0300

    Criacao aula09.cpp

---
+++ aulas - parte 1/aula 09 - IF ELSE Parte 1/aula09.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int num = 10;
+    int num2 = 50;
+    char opc = 's';
+
+    /*OPERADORES DE COMPARACAO
+    Maior do que        >
+    Menor do que        <
+    Maior ou igual      >=
+    Menor ou igual      <=
+    Igualdade           ==
+    Diferente           !=
+    */
+
+    if (num < num2)
+    {
+        cout << "Resultado VERDADEIRO";
+    } else {
+        cout << "Resultado FALSO";
+    }
+    
+    return 0;
+}
