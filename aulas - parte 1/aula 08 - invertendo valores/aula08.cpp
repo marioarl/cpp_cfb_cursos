984caae2825374b6ef77958b053cc3fab5b515a6
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-07 16:16:26 -0300

    Criacao aula08.cpp

---
+++ aulas - parte 1/aula 08 - invertendo valores/aula08.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int num = 10;
+    cout << num << "\n\n";
+
+    //num = num * -1; //Invertendo o valor da variavel num (forma padrao)
+    num = -num; //Forma simplificada de inversao da variavel
+
+    cout << num << "\n\n";
+
+    cout << -num << "\n\n";// Operador de inversao, mas ele nao grava na variavel
+
+
+
+    return 0;
+}
