154649f71730336462b77de0c2a410dc16fb7cca
Mario Augusto Lima <81307784+marioarl@users.noreply.github.com>
2022-01-21 11:30:05 -0300

    Criacao aula29.cpp

---
+++ aulas - parte 1/aula 28 - Pilha Stack P1/aula28.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <stack> //Biblioteca para usar o STACK
+
+using namespace std;
+
+/*
+STACK - É uma estrutura de armazenamento de dados, como se fosse um vetor, só que tem controles especificos.
+
+O 1o. elemento a ser inserido, é o ultimo elemento a ser removido ou trabalhado
+
+__      __
+  |  7  |
+  |  6  |
+  |  5  |
+  |  4  |
+  |  3  |
+  |  2  |
+  |__1__|
+
+*/
+
+int main(int argc, char const *argv[])
+{
+    //Criar a pilha
+    stack <string> cartas; // Modelo STACK seguido entre <> do tipo e o nome do STACK
+
+
+    //Incluir itens na pilha 
+    cartas.push("Rei de Copas"); 
+    cartas.push("Rei de Espadas");
+    cartas.push("Rei de Ouros");
+    cartas.push("Rei de Paus");
+
+    cout <<"Tamanho da pilha: " << cartas.size() <<"\n"; //Saida : Tamanho 4
+
+    cout << "Carta do topo: " << cartas.top() << "\n"; //Informa qual o elemento que está no topo
+
+    //Retirar elementos da pilha
+    cartas.pop(); //Retira o elemento do topo - Rei de Paus
+    cout <<"Nova carta do topo: " << cartas.top() <<"\n"; 
+
+
+
+
+    return 0;
+}
